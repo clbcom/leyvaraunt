2763abd7f2861378181fda5e16e01de5cecf6ee8
Cristhian Lima Blanco <=>
2023-06-23 00:29:06 -0400

    se eliminaron lineas de imports innecesarios

--- src/main/java/com/leyvaraunt/routes/Login.java
+++ src/main/java/com/leyvaraunt/routes/Login.java
@@ -10,9 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import com.leyvaraunt.config.Constants;
 import com.leyvaraunt.controllers.ManagerController;
 
 /**
--- src/main/java/com/leyvaraunt/routes/Main.java
+++ src/main/java/com/leyvaraunt/routes/Main.java
@@ -5,19 +5,15 @@
 package com.leyvaraunt.routes;
 
 import java.io.IOException;
-import java.util.Enumeration;
 
-import javax.persistence.Enumerated;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.leyvaraunt.config.Constants;
-import com.leyvaraunt.entities.Manager;
 
 /**
  *
